015a4f7b1a4c0e606eedc27fb82f7d743d2a04f4
Kirill Stytsenko <kirstyts@gmail.com>
2019-03-25 22:05:14 +0100

    A way to extend the webpack config (currently unused)

---
+++ webpack.config.js
@@ -0,0 +1,7 @@
+const webpackConfig = require('./node_modules/@ionic/app-scripts/config/webpack.config')
+
+// single file bundle, instead of 3
+// webpackConfig.prod.output.filename = 'bundle.js'
+// console.log(webpackConfig)
+
+module.exports = webpackConfig
